9d2f6d583abef8d50de1c2a39c9593e77404bdbd
Vanshiii29 <138807197+Vanshiii29@users.noreply.github.com>
2023-11-05 11:41:51 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,54 @@
-import "react-native-gesture-handler";
+const fastify = require("fastify")({ logger: true });
+const fetch = require("node-fetch");
+require("dotenv").config();
 
-import { AppRegistry } from "react-native";
-import App from "./App";
+const COMETCHAT_APP_ID = process.env.COMETCHAT_APP_ID;
+const COMETCHAT_API_KEY = process.env.COMETCHAT_API_KEY;
 
-import { CometChat } from "@cometchat-pro/react-native-chat";
+fastify.get("/", async (request, reply) => {
+	return { hello: "world" };
+});
 
-import Config from "react-native-config";
+fastify.post("/create-user", async (request, reply) => {
+	const { uid, name, email, gender } = request.body;
 
-var appID = Config.COMETCHAT_APP_ID;
-var region = Config.COMETCHAT_APP_REGION;
+	const avatar = encodeURI(`https://robohash.org/${name}`);
+	const url = "https://api-us.cometchat.io/v2.0/users";
+	const metadata = {
+		gender,
+	};
 
-var appSetting = new CometChat.AppSettingsBuilder()
-	.subscribePresenceForAllUsers()
-	.setRegion(region)
-	.build();
-CometChat.init(appID, appSetting).then(
-	() => {
-		console.log("Initialization completed successfully");
-	},
-	(error) => {
-		console.log("Initialization failed with error:", error);
+	const options = {
+		method: "POST",
+		body: JSON.stringify({
+			uid,
+			name,
+			email,
+			avatar,
+			metadata: JSON.stringify(metadata),
+		}),
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+			appId: COMETCHAT_APP_ID,
+			apiKey: COMETCHAT_API_KEY,
+		},
+	};
+
+	fetch(url, options)
+		.then((res) => res.json())
+		.then((json) => console.log(json))
+		.catch((err) => console.error("error:" + err));
+
+	return "ok";
+});
+
+const start = async () => {
+	try {
+		await fastify.listen(3000);
+	} catch (err) {
+		fastify.log.error(err);
+		process.exit(1);
 	}
-);
-
-import { name as appName } from "./app.json";
-
-AppRegistry.registerComponent(appName, () => App);
+};
+start();
